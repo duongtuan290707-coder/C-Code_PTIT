32367f13db8ae7627b1b2c5d2a2dff197d517c78
Tuan <duongtuan290707@gmail.com>
2026-04-02 09:16:52 +0700

    P

--- Tim_solonnhat.c
+++ Tim_solonnhat.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main() {
-    long long n,max;
-    
+int main(){
+    long long n,maxx;
+    maxx=-1;
+    while(scanf("%lld",&n) ==1){
+        if(maxx<n) maxx=n;
+    }
+    printf("%lld",maxx);
     return 0;
 }
