4f2a3935fd6c2329050864a84161fbf6f4a8afc1
Tuan <duongtuan290707@gmail.com>
2026-04-06 17:43:17 +0700

    lap

---
+++ C01027.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int A ( int a, int b) {
+    while( b != 0) {
+        int r = a%b;
+        a = b;
+        b = r;
+    } return a;
+}
+int main() {
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("%d\n", A(a, b));
+    } return 0;
+}
